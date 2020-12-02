352f4fc5dfd6373072881c6fe0f9edfd475226ce
Nathan Chiu <70767147+nathan-stytch@users.noreply.github.com>
2020-12-01 18:23:16 -0600

    Setup.py + Integration tests (#3)
    * initial commit for integration tests
    
    * Add release files
    
    * Initial commit for integration test
    
    * Fixes for some tests
    
    * PR comments

--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup
 
-setup(name="PACKAGENAME", packages=find_packages())
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="stytch",
+    version="1.0.0",
+    description="Stytch python client",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/stytchauth/stytch-python",
+    keywords=[
+        "stytch",
+        "user",
+        "authentication",
+    ],
+    author="Stytch",
+    author_email="hello@stytch.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=["requests>=2.7.0"],
+)
