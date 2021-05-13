3de8e88b87c70b485226578688e7932040494bfc
Jeremy Kaplan <jeremy@stytch.com>
2021-05-13 12:50:51 -0700

    Fix attributes parameter in Users.create (#28)
    I noticed that `Client.Users.create` fails when including attributes because they get added into the root of the request data instead of under the "attributes" key.  This change fixes that.
    
    This problem isn't present on other endpoints, so I copied the data merge pattern from there.

---
+++ tests/api/users_test.py
@@ -0,0 +1,47 @@
+import json
+import pytest
+import requests
+from unittest import mock
+
+from stytch.api.users import Users
+
+
+class FakeClient:
+    def __init__(self):
+        self.base_url = "https://localhost:8080/"
+        self.project_id = "NO_PROJECT_ID"
+        self.secret = "NO_SECRET"
+
+
+class FakeResponse:
+    def __init__(self, status_code=None, body="{}"):
+        self.status_code = status_code
+        self.body = body
+
+    def json(self):
+        return json.loads(self.body)
+
+
+class TestUsers:
+    def test_create_attributes(self):
+        response = FakeResponse(
+            status_code=200,
+            body='{"user_id": "user-test-183e939c-e7e1-4d56-82da-8ba9c6036878"}',
+        )
+        with mock.patch.object(requests, "post", return_value=response) as mock_post:
+            users = Users(FakeClient())
+            users._requester_base = requests
+            response = users.create(
+                email="test@example.net",
+                attributes={
+                    "ip_address": "203.0.113.1",
+                    "user_agent": "Toaster 3.0",
+                },
+            )
+
+        mock_post.assert_called_once_with(
+            "https://localhost:8080/users",
+            data='{"email": "test@example.net", "phone_number": null, "name": {"first_name": null, "middle_name": null, "last_name": null}, "attributes": {"ip_address": "203.0.113.1", "user_agent": "Toaster 3.0"}}',
+            auth=mock.ANY,
+            headers=mock.ANY,
+        )
