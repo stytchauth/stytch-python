349f69f5159d393614ea25ef55de4c223d32ffa5
alex-stytch <alex@stytch.com>
2021-05-28 09:45:30 -0700

    Update CreateUser and magic_link_url endpoints

--- stytch/api/magic_links.py
+++ stytch/api/magic_links.py
@@ -30,19 +30,24 @@ class MagicLinks(Base):
         self,
         method_id: str,
         user_id: str,
-        magic_link_url: str,
-        expiration_minutes: Optional[int] = None,
+        login_magic_link_url: str,
+        signup_magic_link_url: str,
+        login_expiration_minutes: Optional[int] = None,
+        signup_expiration_minutes: Optional[int] = None,
         attributes: Optional[Dict] = None,
     ):
         attributes = self._validate_attributes(attributes)
         data = {
             "method_id": method_id,
             "user_id": user_id,
-            "magic_link_url": magic_link_url,
+            "login_magic_link_url": login_magic_link_url,
+            "signup_magic_link_url": signup_magic_link_url,
             "attributes": attributes,
         }
-        if expiration_minutes:
-            data["expiration_minutes"] = expiration_minutes
+        if login_expiration_minutes:
+            data["login_expiration_minutes"] = login_expiration_minutes
+        if signup_expiration_minutes:
+            data["signup_expiration_minutes"] = signup_expiration_minutes
 
         return self._post(
             "{0}/send".format(
@@ -54,18 +59,23 @@ class MagicLinks(Base):
     def send_by_email(
         self,
         email: str,
-        magic_link_url: str,
-        expiration_minutes: Optional[int] = None,
+        login_magic_link_url: str,
+        signup_magic_link_url: str,
+        login_expiration_minutes: Optional[int] = None,
+        signup_expiration_minutes: Optional[int] = None,
         attributes: Optional[Dict] = None,
     ):
         attributes = self._validate_attributes(attributes)
         data = {
             "email": email,
-            "magic_link_url": magic_link_url,
+            "login_magic_link_url": login_magic_link_url,
+            "signup_magic_link_url": signup_magic_link_url,
             "attributes": attributes,
         }
-        if expiration_minutes:
-            data["expiration_minutes"] = expiration_minutes
+        if login_expiration_minutes:
+            data["login_expiration_minutes"] = login_expiration_minutes
+        if signup_expiration_minutes:
+            data["signup_expiration_minutes"] = signup_expiration_minutes
 
         return self._post(
             "{0}/send_by_email".format(
@@ -82,50 +92,59 @@ class MagicLinks(Base):
         login_expiration_minutes: Optional[int] = None,
         signup_expiration_minutes: Optional[int] = None,
         attributes: Optional[Dict] = None,
-        create_user_as_pending: Optional[bool] = None,
+        create_user_as_pending: Optional[bool] = False,
     ):
         attributes = self._validate_attributes(attributes)
+        data = {
+           "email": email,
+           "login_magic_link_url": login_magic_link_url,
+           "signup_magic_link_url": signup_magic_link_url,
+           "attributes": attributes,
+           "create_user_as_pending": create_user_as_pending,
+       }
+
+        if login_expiration_minutes:
+            data["login_expiration_minutes"] = login_expiration_minutes
+        if signup_expiration_minutes:
+            data["signup_expiration_minutes"] = signup_expiration_minutes
+
         return self._post(
             "{0}/login_or_create".format(
                 self.magic_link_url,
             ),
-            data={
-                "email": email,
-                "login_magic_link_url": login_magic_link_url,
-                "signup_magic_link_url": signup_magic_link_url,
-                "login_expiration_minutes": login_expiration_minutes,
-                "signup_expiration_minutes": signup_expiration_minutes,
-                "attributes": attributes,
-                "create_user_as_pending": create_user_as_pending,
-            },
+            data=data,
         )
 
     def invite_by_email(
         self,
         email: str,
-        magic_link_url: str,
-        expiration_minutes: Optional[int] = None,
+        invite_magic_link_url: str,
+        invite_expiration_minutes: Optional[int] = None,
         attributes: Optional[Dict] = None,
         first_name: Optional[str] = None,
         last_name: Optional[str] = None,
         middle_name: Optional[str] = None,
     ):
         attributes = self._validate_attributes(attributes)
+        data = {
+            "email": email,
+            "invite_magic_link_url": invite_magic_link_url,
+            "attributes": attributes,
+            "name": {
+                "first_name": first_name,
+                "middle_name": middle_name,
+                "last_name": last_name,
+            },
+        }
+
+        if invite_expiration_minutes:
+            data["invite_expiration_minutes"] = invite_expiration_minutes
+
         return self._post(
             "{0}/invite_by_email".format(
                 self.magic_link_url,
             ),
-            data={
-                "email": email,
-                "magic_link_url": magic_link_url,
-                "expiration_minutes": expiration_minutes,
-                "attributes": attributes,
-                "name": {
-                    "first_name": first_name,
-                    "middle_name": middle_name,
-                    "last_name": last_name,
-                },
-            },
+            data=data,
         )
 
     def revoke_invite_by_email(
