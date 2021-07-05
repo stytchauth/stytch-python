a60bbb3fbe5e4f37bbe992056aab7a6e49644487
alex-stytch <alex@stytch.com>
2021-07-05 16:01:53 -0700

    Add WhatsApp endpoints

--- stytch/api/otp.py
+++ stytch/api/otp.py
@@ -6,6 +6,7 @@ class OTP(Base):
     def __init__(self, client):
         super().__init__(client)
         self.sms = SMS(client)
+        self.whatsapp = Whatsapp(client)
 
     @property
     def otp_url(self):
@@ -82,6 +83,57 @@ class SMS(Base):
 
         return self._post(
             "{0}/sms/login_or_create".format(
+                self.otp_url,
+            ),
+            data=data,
+        )
+
+class Whatsapp(Base):
+    @property
+    def otp_url(self):
+        return self.get_url("otps")
+
+    def send(
+        self,
+        phone_number: str,
+        expiration_minutes: Optional[int] = None,
+        attributes: Optional[Dict] = None,
+    ):
+        attributes = _validate_attributes(attributes)
+        data = {
+            "phone_number": phone_number,
+        }
+        if expiration_minutes:
+            data["expiration_minutes"] = expiration_minutes
+        if attributes:
+            data["attributes"] = attributes
+
+        return self._post(
+            "{0}/whatsapp/send".format(
+                self.otp_url,
+            ),
+            data=data,
+        )
+
+    def login_or_create(
+        self,
+        phone_number: str,
+        expiration_minutes: Optional[int] = None,
+        attributes: Optional[Dict] = None,
+        create_user_as_pending: Optional[bool] = False
+    ):
+        attributes = _validate_attributes(attributes)
+        data = {
+            "phone_number": phone_number,
+            "create_user_as_pending": create_user_as_pending,
+        }
+        if expiration_minutes:
+            data["expiration_minutes"] = expiration_minutes
+        if attributes:
+            data["attributes"] = attributes
+
+        return self._post(
+            "{0}/whatsapp/login_or_create".format(
                 self.otp_url,
             ),
             data=data,
