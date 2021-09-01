a16ff330f8fc762492596fca473d01c5c24affd6
Alex Zaldastani <73447029+alex-stytch@users.noreply.github.com>
2021-09-01 16:14:59 -0700

    Add Email OTP

--- tests/api/users_test.py
+++ tests/api/users_test.py
@@ -45,6 +45,6 @@ class TestUsers:
             auth=users.auth,
             headers={
                 'Content-Type': 'application/json',
-                'User-Agent': 'Stytch Python v4.2.0'
+                'User-Agent': 'Stytch Python v4.3.0'
             },
         )
