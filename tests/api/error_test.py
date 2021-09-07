942b33647cc18b2682d17118f93f9cc7ed3d0460
Nikhil Dilip <nikhil@stytch.com>
2021-09-07 12:14:46 -0700

    add status_code to StytchError

--- tests/api/error_test.py
+++ tests/api/error_test.py
@@ -10,7 +10,8 @@ class TestStytchError:
           "request_id": "request-id-test-59331c7a-e4d6-41dc-aa3f-65f5cc3fdfdc",
           "error_type": "invalid_email",
           "error_message": "Email format is invalid",
-          "error_url": "https://stytch.com/docs/api/errors/400"
+          "error_url": "https://stytch.com/docs/api/errors/400",
+          "status_code": 400
         }
         """
 
