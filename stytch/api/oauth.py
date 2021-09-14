7ea51d3d8d60d69b93098a40f604ee025da4bafe
Danny Thomson <danny@stytch.io>
2021-09-13 22:28:43 -0700

    Add OAuth Authenticate

---
+++ stytch/api/oauth.py
@@ -0,0 +1,32 @@
+from typing import Dict, Optional
+from enum import Enum
+
+from .base import _validate_attributes, Base
+
+class SessionManagementType(Enum):
+    IDP = "idp"
+    STYTCH = "stytch"
+    NONE = "none"
+
+class OAuth(Base):
+    def __init__(self, client):
+        super().__init__(client)
+
+    @property
+    def oauth_url(self):
+        return self.get_url("oauth")
+
+    def authenticate(
+        self,
+        token: str,
+        session_management_type: SessionManagementType,
+    ):
+
+        data={
+            "token": token,
+            "session_management_type": session_management_type,
+        }
+        return self._post(
+            "{0}/authenticate".format(self.oauth_url),
+            data=data,
+        )
