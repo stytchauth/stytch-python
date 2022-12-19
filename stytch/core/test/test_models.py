1feedee7f7b08fd7d9d08d988da86fd5ca8d9c02
Jeremy Kaplan <jeremy@stytch.com>
2022-12-19 11:20:52 -0800

    Add a test for StytchError details (#99)

--- stytch/core/test/test_models.py
+++ stytch/core/test/test_models.py
@@ -108,3 +108,18 @@ class TestModels(unittest.TestCase):
             mock_details = create_autospec(models.StytchErrorDetails)
             str(models.StytchError(mock_details))
             mock_details.__str__.assert_called_once()
+
+    def test_stytcherror_fields(self) -> None:
+        resp = {
+          "status_code": 418,
+          "request_id": "request-id-test-fea11c44-5514-4aac-a76b-3ca685e3443a",
+          "error_type": "is_a_teapot",
+          "error_message": "I'm a teapot!",
+          "error_url": "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418",
+        }
+        expected = models.StytchErrorDetails(**resp)
+
+        with self.assertRaises(models.StytchError) as e:
+            DummyResponse.from_json(418, resp)
+
+        self.assertEqual(e.exception.details, expected)
