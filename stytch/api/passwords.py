3c6b7cbfd04cb672cac84ba0c99ffd6c1d3db661
alex-stytch <alex@stytch.com>
2023-03-28 14:00:10 -0700

    Add name and metadata to B2C Password endpoints

--- stytch/api/passwords.py
+++ stytch/api/passwords.py
@@ -4,7 +4,9 @@
 # or your changes may be overwritten later!
 # !!!
 
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional, Union
+
+import pydantic
 
 from stytch.api.passwords_email import Email
 from stytch.api.passwords_existing_password import ExistingPassword
@@ -41,8 +43,11 @@ class Passwords:
         self,
         email: str,
         password: str,
+        name: Optional[Union[Name, Dict[str, str]]] = None,
         session_duration_minutes: Optional[int] = None,
         session_custom_claims: Optional[Dict[str, Any]] = None,
+        trusted_metadata: Optional[Dict[str, Any]] = None,
+        untrusted_metadata: Optional[Dict[str, Any]] = None,
     ) -> CreateResponse:
         """[Stytch docs](https://stytch.com/docs/api/password-create)
 
@@ -58,10 +63,16 @@ class Passwords:
             "password": password,
         }
 
+        if name is not None:
+            payload["name"] = name
         if session_duration_minutes is not None:
             payload["session_duration_minutes"] = session_duration_minutes
         if session_custom_claims is not None:
             payload["session_custom_claims"] = session_custom_claims
+        if trusted_metadata is not None:
+            payload["trusted_metadata"] = trusted_metadata
+        if untrusted_metadata is not None:
+            payload["untrusted_metadata"] = untrusted_metadata
 
         url = self.api_base.route_with_sub_url(self.sub_url, None)
 
@@ -69,11 +80,14 @@ class Passwords:
         return CreateResponse.from_json(res.response.status_code, res.json)
 
     async def create_async(
-        self,
-        email: str,
-        password: str,
-        session_duration_minutes: Optional[int] = None,
-        session_custom_claims: Optional[Dict[str, Any]] = None,
+      self,
+      email: str,
+      password: str,
+      name: Optional[Union[Name, Dict[str, str]]] = None,
+      session_duration_minutes: Optional[int] = None,
+      session_custom_claims: Optional[Dict[str, Any]] = None,
+      trusted_metadata: Optional[Dict[str, Any]] = None,
+      untrusted_metadata: Optional[Dict[str, Any]] = None,
     ) -> CreateResponse:
         """[Stytch docs](https://stytch.com/docs/api/password-create)
 
@@ -89,10 +103,16 @@ class Passwords:
             "password": password,
         }
 
+        if name is not None:
+            payload["name"] = name
         if session_duration_minutes is not None:
             payload["session_duration_minutes"] = session_duration_minutes
         if session_custom_claims is not None:
             payload["session_custom_claims"] = session_custom_claims
+        if trusted_metadata is not None:
+            payload["trusted_metadata"] = trusted_metadata
+        if untrusted_metadata is not None:
+            payload["untrusted_metadata"] = untrusted_metadata
 
         url = self.api_base.route_with_sub_url(self.sub_url, None)
 
@@ -141,13 +161,13 @@ class Passwords:
         return AuthenticateResponse.from_json(res.response.status_code, res.json)
 
     async def authenticate_async(
-        self,
-        email: str,
-        password: str,
-        session_token: Optional[str] = None,
-        session_jwt: Optional[str] = None,
-        session_duration_minutes: Optional[int] = None,
-        session_custom_claims: Optional[Dict[str, Any]] = None,
+      self,
+      email: str,
+      password: str,
+      session_token: Optional[str] = None,
+      session_jwt: Optional[str] = None,
+      session_duration_minutes: Optional[int] = None,
+      session_custom_claims: Optional[Dict[str, Any]] = None,
     ) -> AuthenticateResponse:
         """[Stytch docs](https://stytch.com/docs/api/password-authenticate)
 
@@ -244,6 +264,9 @@ class Passwords:
     def migrate(
         self,
         email: str,
+        name: Optional[Union[Name, Dict[str, str]]] = None,
+        trusted_metadata: Optional[Dict[str, Any]] = None,
+        untrusted_metadata: Optional[Dict[str, Any]] = None,
         hash: str,
         hash_type: str,
         md_5_config: Optional[Dict[str, Any]] = None,
@@ -262,6 +285,12 @@ class Passwords:
             "hash_type": hash_type,
         }
 
+        if name is not None:
+            payload["name"] = name
+        if trusted_metadata is not None:
+            payload["trusted_metadata"] = trusted_metadata
+        if untrusted_metadata is not None:
+            payload["untrusted_metadata"] = untrusted_metadata
         if md_5_config is not None:
             payload["md_5_config"] = md_5_config
         if argon_2_config is not None:
@@ -277,14 +306,17 @@ class Passwords:
         return MigrateResponse.from_json(res.response.status_code, res.json)
 
     async def migrate_async(
-        self,
-        email: str,
-        hash: str,
-        hash_type: str,
-        md_5_config: Optional[Dict[str, Any]] = None,
-        argon_2_config: Optional[Dict[str, Any]] = None,
-        sha_1_config: Optional[Dict[str, Any]] = None,
-        scrypt_config: Optional[Dict[str, Any]] = None,
+      self,
+      email: str,
+      name: Optional[Union[Name, Dict[str, str]]] = None,
+      trusted_metadata: Optional[Dict[str, Any]] = None,
+      untrusted_metadata: Optional[Dict[str, Any]] = None,
+      hash: str,
+      hash_type: str,
+      md_5_config: Optional[Dict[str, Any]] = None,
+      argon_2_config: Optional[Dict[str, Any]] = None,
+      sha_1_config: Optional[Dict[str, Any]] = None,
+      scrypt_config: Optional[Dict[str, Any]] = None,
     ) -> MigrateResponse:
         """[Stytch docs](https://stytch.com/docs/api/password-migrate)
 
@@ -297,6 +329,12 @@ class Passwords:
             "hash_type": hash_type,
         }
 
+        if name is not None:
+            payload["name"] = name
+        if trusted_metadata is not None:
+            payload["trusted_metadata"] = trusted_metadata
+        if untrusted_metadata is not None:
+            payload["untrusted_metadata"] = untrusted_metadata
         if md_5_config is not None:
             payload["md_5_config"] = md_5_config
         if argon_2_config is not None:
