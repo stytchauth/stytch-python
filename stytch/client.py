181ea86af3626a70846f6439ab108e84bcdcc3f3
Logan Gore <lgore@stytch.com>
2023-05-26 11:56:40 -0700

    Infer environment from project_id

--- stytch/client.py
+++ stytch/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import warnings
+from typing import Optional
 
 from stytch.api.crypto_wallets import CryptoWallets
 from stytch.api.magic_links import MagicLinks
@@ -26,10 +27,10 @@ class Client:
         self,
         project_id: str,
         secret: str,
-        environment: str,
+        environment: Optional[str] = None,
         suppress_warnings: bool = False,
     ):
-        base_url = self._env_url(environment, suppress_warnings)
+        base_url = self._env_url(project_id, environment, suppress_warnings)
         api_base = ApiBase(base_url)
         sync_client = SyncClient(project_id, secret)
         async_client = AsyncClient(project_id, secret)
@@ -45,15 +46,28 @@ class Client:
         self.passwords = Passwords(api_base, sync_client, async_client)
 
     @classmethod
-    def _env_url(cls, env: str, suppress_warnings: bool = False) -> str:
+    def _env_url(
+        cls, project_id: str, env: Optional[str] = None, suppress_warnings: bool = False
+    ) -> str:
         """Resolve the base URL for the Stytch API environment."""
+        live_api = "https://api.stytch.com/v1/"
+        test_api = "https://test.stytch.com/v1/"
+        test_warning = "Test version of Stytch not intended for production use"
+
+        if env is None:
+            if project_id.startswith("project-live-"):
+                return live_api
+            else:
+                if not suppress_warnings:
+                    warnings.warn(test_warning)
+                return test_api
 
         # Supported production environments
         if env == "test":
             if not suppress_warnings:
-                warnings.warn("Test version of Stytch not intended for production use")
-            return "https://test.stytch.com/v1/"
+                warnings.warn(test_warning)
+            return test_api
         elif env == "live":
-            return "https://api.stytch.com/v1/"
+            return live_api
 
         return env
