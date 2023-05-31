4aafb7e291a1ebce0cdd97bd2c285a5239825bda
Logan Gore <119902778+logan-stytch@users.noreply.github.com>
2023-05-31 12:23:11 -0700

    Infer environment from project_id (#136)
    * Infer environment from project_id
    
    * Bump version
    
    * Update client test

--- stytch/b2b/client.py
+++ stytch/b2b/client.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import warnings
+from typing import Optional
 
+from stytch.b2b.api.discovery import Discovery
 from stytch.b2b.api.magic_links import MagicLinks
 from stytch.b2b.api.organizations import Organizations
 from stytch.b2b.api.passwords import Passwords
 from stytch.b2b.api.sessions import Sessions
-from stytch.b2b.api.discovery import Discovery
 from stytch.core.api_base import ApiBase
 from stytch.core.http.client import AsyncClient, SyncClient
 
@@ -22,10 +23,10 @@ class Client:
         self,
         project_id: str,
         secret: str,
-        environment: str,
+        environment: Optional[str] = None,
         suppress_warnings: bool = False,
     ):
-        base_url = self._env_url(environment, suppress_warnings)
+        base_url = self._env_url(project_id, environment, suppress_warnings)
         api_base = ApiBase(base_url)
         sync_client = SyncClient(project_id, secret)
         async_client = AsyncClient(project_id, secret)
@@ -37,15 +38,28 @@ class Client:
         self.discovery = Discovery(api_base, sync_client, async_client)
 
     @classmethod
-    def _env_url(cls, env: str, suppress_warnings: bool = False) -> str:
+    def _env_url(
+        cls, project_id: str, env: Optional[str] = None, suppress_warnings: bool = False
+    ) -> str:
         """Resolve the base URL for the Stytch API environment."""
+        live_api = "https://api.stytch.com/v1/b2b/"
+        test_api = "https://test.stytch.com/v1/b2b/"
+        test_warning = "Test version of Stytch not intended for production use"
+
+        if env is None:
+            if project_id.startswith("project-live-"):
+                return live_api
+            else:
+                if not suppress_warnings:
+                    warnings.warn(test_warning)
+                return test_api
 
         # Supported production environments
         if env == "test":
             if not suppress_warnings:
-                warnings.warn("Test version of Stytch not intended for production use")
-            return "https://test.stytch.com/v1/b2b/"
+                warnings.warn(test_warning)
+            return test_api
         elif env == "live":
-            return "https://api.stytch.com/v1/b2b/"
+            return live_api
 
         return env
--- stytch/test/test_client.py
+++ stytch/test/test_client.py
@@ -5,20 +5,24 @@ import unittest.mock
 
 from stytch.client import Client
 
+TEST_PROJECT_ID = "project-test-11111111-2222-3333-4444-555555555555"
+LIVE_PROJECT_ID = "project-live-66666666-7777-8888-9999-000000000000"
+
 
 class TestClient(unittest.TestCase):
     # Logically private methods
     def test__env_url(self) -> None:
         # Arrange
         env_with_expected = [
-            ("test", "https://test.stytch.com/v1/"),
-            ("live", "https://api.stytch.com/v1/"),
-            ("something_else", "something_else"),
+            (TEST_PROJECT_ID, "test", "https://test.stytch.com/v1/"),
+            (LIVE_PROJECT_ID, "live", "https://api.stytch.com/v1/"),
+            (LIVE_PROJECT_ID, "something_else", "something_else"),
+            (LIVE_PROJECT_ID, None, "https://api.stytch.com/v1/"),
         ]
 
-        for env, expected in env_with_expected:
+        for project_id, env, expected in env_with_expected:
             # Act
             with unittest.mock.patch("stytch.client.warnings", autospec=True):
-                actual = Client._env_url(env)
+                actual = Client._env_url(project_id=project_id, env=env)
             # Assert
             self.assertEqual(expected, actual)
